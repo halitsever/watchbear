997e8a78b53ee66551bf94e16af58a29404a7820
Halit Sever <halitsever@protonmail.com>
2026-06-15 23:34:43 +0300

    feat: bump version to 0.1.1

--- apps/extension/manifest.config.ts
+++ apps/extension/manifest.config.ts
@@ -12,7 +12,7 @@ export default defineManifest({
   name: 'Watchbear',
   short_name: 'Watchbear',
   description: 'The sweet way to watch together',
-  version: '0.1.0',
+  version: '0.1.1',
   icons,
   action: {
     default_popup: 'src/popup/index.html',
